9a6dfdd106d0aab33a707acece18eb2827c0a16f
Aditya Raj Chaudhary <adi@Adityas-MacBook-Air.local>
2025-09-11 10:48:08 +0530

    added new files

--- Day3/Variables.java
+++ Day3/Variables.java
@@ -4,5 +4,21 @@ public class Variables {
     public static void main(String[] args) {
         int number = 5;
         System.out.println("Number: " + number);
+        final double PI = 3.14;
+        System.out.println("PI: " + PI);
+        byte b = 10;
+        System.out.println("Byte: " + b);
+        short s = 1000;
+        System.out.println("Short: " + s);
+        long l = 1000000000000000000L;
+        System.out.println("Long: " + l);
+        float f = 10.5F;
+        System.out.println("Float: " + f);
+        double d = 10.5;
+        System.out.println("Double: " + d);
+        char c = 'A';
+        System.out.println("Char: " + c);
+        boolean bool = true;
+        System.out.println("Boolean: " + bool);
     }
 }
