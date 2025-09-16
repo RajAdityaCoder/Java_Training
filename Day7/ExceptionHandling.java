bbc7f1fb927967a4a14042fc72467da67d3d976d
Aditya Raj Chaudhary <adi@Adityas-MacBook-Air.local>
2025-09-16 10:38:31 +0530

    added new files

---
+++ Day7/ExceptionHandling.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        try {
+            int c = a / 0;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println("You can't divide by zero");
+        }
+    }
+}
