d4ff212a8d4de3789331a61749c5c37fa1a50aaf
Aditya Raj Chaudhary <adi@Adityas-MacBook-Air.local>
2025-09-16 10:13:27 +0530

    added files

---
+++ Day7/Interfaces.java
@@ -0,0 +1,42 @@
+
+interface Animal {
+
+    void eat();
+
+    void sleep();
+}
+
+class Dog implements Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Dog is sleeping");
+    }
+}
+
+class Cat implements Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("Cat is eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Cat is sleeping");
+    }
+
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        dog.eat();
+        dog.sleep();
+        Animal cat = new Cat();
+        cat.eat();
+        cat.sleep();
+    }
+}
